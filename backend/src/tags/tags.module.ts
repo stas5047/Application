29e748c66be5ab4bd48c243e5784c4925b24ccca
Kotletka <stasik5047@gmail.com>
2026-03-11 14:18:39 +0200

    added Tag entity, event_tags junction table and migration, register TagsModule in AppModule, extend Event entity with tags ManyToMany relation eager false

---
+++ backend/src/tags/tags.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
+})
+export class TagsModule {}
